b5ceef0fe376234e12d40b0c9976b8640bc71df4
Jadchv <chvjade5@gmail.com>
2025-05-18 19:41:32 +0200

    fix bugs

--- Front/src/component/ScrollToTop.jsx
+++ Front/src/component/ScrollToTop.jsx
@@ -4,19 +4,12 @@ import { useLocation } from "react-router-dom";
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
-  // Liste des chemins à exclure (peut être une route dynamique)
-  const excludedPaths = [/^\/recipes\/\d+$/]; // regex qui matche /recipes/123
-
-  const isExcluded = excludedPaths.some((pattern) => pattern.test(pathname));
-
   useEffect(() => {
-    if (!isExcluded) {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
-  }, [pathname, isExcluded]);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
 
   return null;
 };
